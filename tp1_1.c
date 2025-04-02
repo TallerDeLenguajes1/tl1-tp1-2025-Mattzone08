be098b290cacd5689722872cae64bda5157928f7
“Matias <mattmedi08@gmail.com>
2025-04-02 11:28:02 -0300

    Commit Con la parte de punteros

--- tp1_1.c
+++ tp1_1.c
@@ -7,6 +7,20 @@ int main(){
     printf("Hello World\n\n");
     printf("Ciao Mondo\n\n");
 
+    int *puntero;
+    int numero = 0;
+
+    puntero = &numero;
+
+    printf("El contenido del puntero es: %d \n\n", *puntero);
+
+    printf("La direccion de memoria almacenada por el puntero es: %p \n\n", puntero);
+
+    printf("La direccion de memoria de la variable es: %p \n\n", &numero);
+
+    printf("La direccion de memoria del puntero es: %p \n\n", &puntero);
+
+    printf("El tamaño de memoria utilizado por la variable es de:  %d Bytes", sizeof(numero));
 
     return 0;
 }
